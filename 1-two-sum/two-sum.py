d8f4f470b0fffef46d55f0f865579cfca5c21c60
tahsinturag <89533695+tahsinturag@users.noreply.github.com>
2024-09-06 13:12:23 +0600

    Time: 64 ms (47.15%) | Memory: 17.7 MB (30.72%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # bruthforce
-        for i in range(len(nums)):                
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # optimal solution using hashmap in c++ / dictionary in python
+        seen = {}
+        for i , value in enumerate (nums):
+            remaining = target - value
+            # remaining = target - nums[i]
+            if remaining in seen: 
+                return[seen[remaining], i]
+            seen[value] = i
+        
 
-
-# T:  0(n^2) 
-# S: O(1)  The solution does not use any additional data structure
+# T: 0(n)  a single loop that iterates through the nums list exactly once, where n is the length of the list.
+# S: O(n)  all n elements could be stored in the seen dictionary
